21a7a0e7a84e9ea52ce5d38cd3ff7264a3e4216b
Carolyn Evans <carolyn.evans@enom.com>
2015-02-07 16:44:49 -0800

    Working on resolve_uri.

--- http_server.py
+++ http_server.py
@@ -7,7 +7,7 @@ def response_ok(body, mimetype):
     """returns a basic HTTP response"""
     resp = []
     resp.append("HTTP/1.1 200 OK")
-    resp.append("Content-Type: text/plain")
+    resp.append(mimetype)
     resp.append("")
     resp.append(body)
     return "\r\n".join(resp)
@@ -20,6 +20,12 @@ def response_method_not_allowed():
     resp.append("")
     return "\r\n".join(resp)
 
+def response_not_found():
+    """returns a 404 Not Found"""
+    resp = []
+    resp.append("HTTP/1.1 404 Not Found")
+    resp.append("")
+    return "\r\n".join(resp)
 
 def parse_request(request):
     first_line = request.split("\r\n", 1)[0]
@@ -30,37 +36,38 @@ def parse_request(request):
     return uri
 
 def build_listing(path):
+    os.chdir(path)
     listing=[]
-    for dirname, dirnames, filenames in os.walk(path):
+    for dirname, dirnames, filenames in os.walk('.'):
         # print path to all subdirectories first.
         for subdirname in dirnames:
-            listing.append(os.path.join(dirname, subdirname))
+            listing.append(subdirname)
 
         # print path to all filenames.
         for filename in filenames:
-            listing.append(os.path.join(dirname, filename))
+            listing.append(filename)
 
-        return listing
+    return listing
 
 def resolve_uri(uri):
-    url = './webroot{}'.format(uri)
+    url = 'webroot{}'.format(uri)
 
     if os.path.isfile(url):
         mimetype = mimetypes.guess_type(uri)[0]
         s = ''
         body = open(url, 'r').read()
-    else:
+    elif os.path.isdir(url):
         mimetype = 'text/plain'
         body = build_listing(url)
+    else:
+        raise NameError("File or Directory not found")
 
-    response_ok(body, mimetype)
+    results = []
+    results.append(body)
+    results.append(mimetype)
+    return results
 
-def response_not_found():
-    """returns a 404 Not Found"""
-    resp = []
-    resp.append("HTTP/1.1 404 Not Found")
-    resp.append("")
-    return "\r\n".join(resp)
+
 
 def server():
     address = ('127.0.0.1', 10000)
@@ -90,15 +97,13 @@ def server():
                 else:
                     # replace this line with the following once you have
                     # written resolve_uri
-                    content, type = resolve_uri(uri)
-                    # content, type = resolve_uri(uri) # change this line
-
-                    ## uncomment this try/except block once you have fixed
-                    ## response_ok and added response_not_found
                     try:
+                        content, type = resolve_uri(uri)
                         response = response_ok(content, type)
+
                     except NameError:
-                        response = response_not_found()
+                            response = response_not_found()
+
 
                 print >>sys.stderr, 'sending response'
                 conn.sendall(response)
